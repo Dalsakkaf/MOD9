3ab8741546704d4bc48d6555e183550d4f2379e8
Dalsakkaf <105419100+Dalsakkaf@users.noreply.github.com>
2022-07-26 20:28:12 -0700

    Added Questions
    Next to add the Functions in Index

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "(title? (required)",
+  "Section Titles? (required)",
+  "Description? (required)",
+  "Table of Content (required)",
+  "How to Install? (required)",
+  "Usage? (required)",
+  "License? (required)",
+  "Who Make it? (required)",
+  "Tests (required)",
+  "Questions (required)",
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
