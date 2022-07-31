23c47ec873bba09f917da2f44d2463a78968162a
Dalsakkaf <105419100+Dalsakkaf@users.noreply.github.com>
2022-07-31 11:43:26 -0700

    Fixed Syntax
    Fixed syntax for questions

--- index.js
+++ index.js
@@ -1,24 +1,72 @@
-// TODO: Include packages needed for this application
+// Packages needed for this are application, Inquirer, fs library package, and the markdown imported from utils
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 const inquirer = require("inquirer");
-// TODO: Create an array of questions for user input
+
+// Array of questions for user input
 const questions = [
-  "Title? (required)",
-  "Section Titles? (required)",
-  "Description? (required)",
-  "Table of Content (required)",
-  "How to Install? (required)",
-  "Usage? (required)",
-  "License? (required)",
-  "Who Make it? (required)",
-  "Tests (required)",
-  "Questions (required)",
+    {    
+        name: "nameOfApp",
+        message: "Title?",
+        type: "input"
+    },
+    {
+        name: "sectionTitles",
+        message: "Section Titles?",
+        type: "input"
+    },
+    {
+        name: "description",
+        message: "Description?",
+        type: "input"
+    },
+    {
+        name: "tableDescription",
+        message: "Description of Table?",
+        type: "input"
+    },
+    {
+        name: "tableContent",
+        message: "What's in the Table of Contents?",
+        type: "input"
+    },
+    {
+        name: "installation",
+        message: "How do you Install the Program?",
+        type: ""
+    },
+    {
+        name: "usage",
+        message: "What to do with it?",
+        type: "input"
+    },
+    {
+        name: "license",
+        message: "what type of license does this app have?",
+        type: "input",
+    },
+    {
+        name: "authors",
+        message: "Who is responsible for creating this?",
+        type: "input"
+    },
+    {
+        name: "tests",
+        message: "How to test it?",
+        type: "input"
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {fs.writeFile(fileName, data, err => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("It is done...")
+})}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {}
 
 // Function call to initialize app
 init();
